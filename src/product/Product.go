5257f1ba6304fbce631935bd6fcdf0eec830ce19
Victor Armando <victorfernando483@gmail.com>
2023-08-11 14:25:29 +0100

    Adding a regexp name validation in Product struct

--- src/product/Product.go
+++ src/product/Product.go
@@ -1,16 +1,25 @@
 package product
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+)
 
 type Product struct {
 	name string
 }
 
 func New(name string) (*Product, error) {
-	if len(name) == 0 {
+	if isNoValidName(name) {
 		return nil, errors.New("Enter the product name!")
 	}
 	return &Product{name}, nil
+}
+
+func isNoValidName(value string) bool {
+	compileNameRegexp := regexp.MustCompile(`^[a-zA-Zãâàáêèéìíîôóòùûú\s]+$`)
+	isValid := compileNameRegexp.MatchString(value)
+	return !isValid
 }
 
 func (product *Product) GetName() string { return product.name }
