3ad078c743f1232077c5d328a753cc9718ac233e
Victor Armando <victorfernando483@gmail.com>
2023-08-11 21:02:47 +0100

    Adding a shopping cart item using builder

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ func main() {
 	newOrder, _ := order.NewOrder(1)
 	firstItem, _ := product.NewProduct("Recarga DSTV Mensal", 7600)
 	secondItem, _ := product.NewProduct("Net Casa UNITEL 10MB/S - 30 Dias", 15000)
-	newOrder.Add(order.NewItem(firstItem, 2, 0))
-	newOrder.Add(order.NewItem(secondItem, 2, 0))
+	newOrder.Add(order.NewItem(firstItem, 2, 0)).Add(order.NewItem(secondItem, 2, 0))
 	fmt.Print(newOrder.ToString())
 }
